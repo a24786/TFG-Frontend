fc5ea495db035081e813cffd2aabfb475acb6231
nunudiewillump <masterdabi@gmail.com>
2021-05-10 18:17:36 +0200

    Vuex, router y store

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
+import VueStore from '@websanova/vue-store';
+
+Vue.use(VueStore);
+Vue.use(Vuex)
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
 new Vue({
+  VueRouter,
+  Vuex,
+  VueStore,
   render: h => h(App),
 }).$mount('#app')
