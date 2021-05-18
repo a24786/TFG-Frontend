497ca1b67192e578ff249e7fccced830d03d27bc
Jaime Aznar Martínez <jaime.aznar70@gmail.com>
2021-05-18 18:43:34 +0200

    commit etructura

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="loginForm">
+    <div class="block1">
+      <img src="../assets/logo_large.png">
+    </div>
+    <div class="block2">
+      <div class="buttons">
+        <button></button>
+        <button></button>
+      </div>
+      <div class="form">
+        <h2>Log in</h2>
+        <form>
+          <input>
+          <input>
+          <button>Log in</button>
+        </form>
+      </div>
+      <div class="privacy_register">
+        <p>No tiene aún una cuenta?<a>Registrate</a></p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'loginForm',
+  props: {
+    msg: String
+  }
+}
+</script>
