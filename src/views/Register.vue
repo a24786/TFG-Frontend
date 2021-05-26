6ca0c855353413ded4ac4c398ad406cb6e0556f8
jaimeAznar <jaime.aznar70@gmail.com>
2021-05-26 20:55:15 +0200

    Quitar header en registro

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,21 +1,16 @@
 <template>
   <div class="register">
-    <Header></Header>
     <RegisterForm></RegisterForm>
-    
   </div>
 </template>
 <script>
 // @ is an alias to /src
 import RegisterForm from '@/components/RegisterForm.vue' 
-import Header from '../components/Header.vue'
 
 export default {
   name: 'register',
   components: {
-     RegisterForm,
-    Header
-
+     RegisterForm
   }
 }
 </script>
