2c3ab9e987a5a56c99117fab41f15d61b41822db
jaimeAznar <jaime.aznar70@gmail.com>
2021-05-28 16:14:58 +0200

    Diseño tarjeta de oferta terminada

--- src/components/OfferCard.vue
+++ src/components/OfferCard.vue
@@ -3,7 +3,10 @@
     <div class="tarjeta">
         <img src="../assets/cafe_croissant.jpg">
         <div class="infoOffer">
-            <h3>J-85</h3>
+            <div class="difuminado">
+              <h3>J-85</h3>
+              <div class="back_difuminado trans"></div>
+            </div>
             <p class="title_offer">Café y croissant</p>
             <p class="price">1,80€</p>
         </div> 
@@ -44,19 +47,20 @@ export default {
 .tarjeta img {
     width: 100%;
     height: 38%;
-    mix-blend-mode: multiply;
+    border-radius: 38px 38px 0px 0px;
     object-fit: cover;
 }
 
 .infoOffer h3 {
     position: absolute;
-    top: 11%;
+    top: 9%;
     left: 8%;
     color: white;
     font-size: 28px;
     font-weight: 600;
-    width: 85%;
+    width: 80%;
     text-align: left;
+    z-index: 1;
 }
 
 .infoOffer .title_offer {
@@ -75,5 +79,19 @@ export default {
 
 .infoOffer {
     display: flex;
+}
+
+.back_difuminado {
+    position: absolute;
+    width: 241px;
+    height: 10px;
+    background-color: black;
+    padding: 10px;
+    top: 24%;
+    box-shadow: 0px -6px 5px 0px black;
+}
+
+.trans {
+    opacity: 0.5;
 }
 </style>
