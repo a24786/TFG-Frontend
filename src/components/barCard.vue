8d6fd594b80ba352666d264fb31e8837e768a873
jaimeAznar <jaime.aznar70@gmail.com>
2021-05-31 14:58:59 +0200

    Responsive1 tajetas bares

--- src/components/barCard.vue
+++ src/components/barCard.vue
@@ -72,7 +72,7 @@ export default {
     font-weight: 600;
     text-align: left;
     padding: 1%;
-    margin-top: -15%;
+    margin-top: -16%;
     margin-left: 28%;
     z-index: 13;
     font-family: 'Roboto';
@@ -92,6 +92,7 @@ export default {
 p.direccion_bar {
     width: 31%;
     margin-left: 29%;
+    margin-top: -3%;
     text-align: left;
     color: grey;
     font-family: 'Roboto';
@@ -113,5 +114,92 @@ p.direccion_bar {
 .button_reservarBar button:hover {
     cursor: pointer;
     background-color: black;
+    border: 0px;
+}
+
+@media only screen and (max-width: 1050px) {
+  .difuminado_bar h3 {
+    margin-top: -17%;
+    font-size: 26px;
+  }
+  .icono_bar img {
+    height: 12vh;
+  }
+  .button_reservarBar button {
+    font-weight: 500;
+    font-size: 20px;
+  }
+  .banner_bar img {
+    height: 13vh;
+  }
+  p.direccion_bar {
+    font-size: 15px;
+  }
+}
+@media only screen and (max-width: 1450px) {
+  .difuminado_bar h3 {
+    margin-top: -16%;
+  }
+  .icono_bar img {
+    height: 17vh;
+  }
+  .tarjetaBar {
+    width: 45%;
+  }
+}
+@media only screen and (max-width: 750px) {
+  .icono_bar img {
+    height: 10vh;
+  }
+  .button_reservarBar button {
+    font-weight: 500;
+    font-size: 16px;
+    margin-bottom: 10px;
+  }
+  .difuminado_bar h3 {
+    margin-top: -20%;
+    font-size: 26px;
+  }
+}
+@media only screen and (max-width: 450px) {
+  .tarjetaBar {
+    width: 85%;
+  }
+  .icono_bar img {
+    height: 12vh;
+    width: 30%;
+    margin-left: -58%;
+    margin-top: -17%;
+  }
+  .difuminado_bar h3 {
+    margin-top: -22%;
+    margin-left: 40%;
+  }
+  .button_reservarBar button {
+    font-weight: 500;
+    font-size: 24px;
+    margin-bottom: 30px;
+    height: 5vh;
+    width: 48%;
+  }
+  p.direccion_bar {
+    font-size: 15px;
+    margin-left: 41%;
+    width: 36%;
+  }
+  .banner_bar img {
+    height: 10vh;
+  }
+}
+@media only screen and (max-height: 750px) {
+  .banner_bar img {
+    height: 14vh;
+  }
+  .icono_bar img {
+    height: 15vh;
+  }
+  .difuminado_bar h3 {
+    margin-top: -17%;
+  }
 }
 </style>
